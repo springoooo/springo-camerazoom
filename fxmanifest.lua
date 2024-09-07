bc629458d607738e432a56aaa56587f92a485325
Springo_dev <mystorp2@gmail.com>
2024-09-08 08:54:42 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- https://github.com/springoooo
+author 'Springo'
+title 'springo-camerazoom'
+description 'Camera Zoom (Prodigy RP 2.0 Inspired)'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
